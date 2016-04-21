83dc9ec66db4bb43e36376e94054b3bf2b312bf9
Александр Прендота <aleksandrprendota@MacBook-Air-Aleksandr.local>
2016-04-21 21:12:45 +0300

    some features

--- GetWeather/ openMapWeather.swift
+++ GetWeather/ openMapWeather.swift
@@ -7,23 +7,115 @@
 //
 
 import Foundation
+import UIKit
 
 
 class openMapWeather {
     var name : String
     var description : String
-    //var temp : Int
+    var temp : Int
+    var wind : Int
+    var currentTime : String?
+    var icon : UIImage?
     
     init(weatherJSON : NSDictionary) {
         
-        let city =  weatherJSON["city"] as! NSDictionary
-        name = city["name"] as! String
 
-        let weather = weatherJSON["list"]![0]!["weather"]!![0] as! NSDictionary
-        description = weather["description"] as! String
-
-       
-       
+        name = weatherJSON["city"]!["name"] as! String
+        description = weatherJSON["list"]![0]!["weather"]!![0]["description"] as! String
+        temp = weatherJSON["list"]![0]!["main"]!!["temp"] as! Int - 273
+        wind = weatherJSON["list"]![0]!["wind"]!!["speed"] as! Int
+        currentTime = timeFromUnix(weatherJSON["list"]![0]!["dt"] as! Int)
+        icon = weatherIcon(weatherJSON["list"]![0]!["weather"]!![0]["icon"] as! String)
         
     }
 }
+
+func timeFromUnix(unixTime : Int) -> String {
+    
+    let timeInSecond = NSTimeInterval(unixTime)
+    let weatherDate = NSDate(timeIntervalSince1970: timeInSecond)
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "HH:mm"
+    
+    return dateFormatter.stringFromDate(weatherDate)
+}
+
+func weatherIcon(stringIcon: String) -> UIImage {
+    
+    let imageName : String
+    
+    switch stringIcon {
+        
+        case "01d" : imageName = "01d"
+        case "02d" : imageName = "02d"
+        case "03d" : imageName = "03d"
+        case "04d" : imageName = "04d"
+        case "09d" : imageName = "09d"
+        case "10d" : imageName = "10d"
+        case "11d" : imageName = "11d"
+        case "13d" : imageName = "13d"
+        case "50d" : imageName = "50d"
+        
+        case "01n" : imageName = "01n"
+        case "02n" : imageName = "02n"
+        case "03n" : imageName = "03n"
+        case "04n" : imageName = "04n"
+        case "09n" : imageName = "09n"
+        case "10n" : imageName = "10n"
+        case "11n" : imageName = "11n"
+        case "13n" : imageName = "13n"
+        case "50n" : imageName = "50n"
+        
+    default: imageName = "none"
+    }
+    
+    let imageIcon = UIImage(named: imageName)
+    return imageIcon!
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- GetWeather/ViewController.swift
+++ GetWeather/ViewController.swift
@@ -12,6 +12,7 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var weatherIcon: UIImageView!
     let url = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=a7a551a913b3979fe01b3e56c05d2a5f"
 
     override func viewDidLoad() {
@@ -25,26 +26,22 @@ class ViewController: UIViewController {
                 let weatherData = NSData(contentsOfURL: stringURL!)
                 do {
                     let weatherJSON = try! NSJSONSerialization.JSONObjectWithData(weatherData!, options:[] ) as! NSDictionary
-            
-                    //print(weatherJSON)
                     let city = openMapWeather(weatherJSON: weatherJSON)
                     
                     print(city.name)
                     print(city.description)
+                    print(city.temp)
+                    print(city.currentTime!)
+                    // Перевод в общий поток!
+                    // Все изменения пользовательского интерфейса производить только в главном потоке.
+                    dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                         self.weatherIcon.image = city.icon!
+                        })
  
                 } catch {
                     print("Fetch failed: \((error as NSError).localizedDescription)")
                 }
-            
-                
-                
-                
-
         }).resume()
-        
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
