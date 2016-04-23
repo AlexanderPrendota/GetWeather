3f37162b2c66c003484c17ce9f8f1ce80f5d58d8
Александр Прендота <aleksandrprendota@MacBook-Air-Aleksandr.local>
2016-04-23 20:16:25 +0300

    add alert

--- GetWeather/ViewController.swift
+++ GetWeather/ViewController.swift
@@ -12,6 +12,9 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    @IBAction func cityTappedButton(sender: UIBarButtonItem) {
+        displayCity()
+    }
     @IBOutlet weak var weatherIcon: UIImageView!
     let url = "http://api.openweathermap.org/data/2.5/forecast/city?q=Moscow&APPID=a7a551a913b3979fe01b3e56c05d2a5f"
     
@@ -50,6 +53,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func displayCity() {
+        
+        let alert = UIAlertController(title: "City", message: "Enter name city", preferredStyle: UIAlertControllerStyle.Alert)
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        let ok = UIAlertAction(title: "Oke", style: UIAlertActionStyle.Default) {
+            (action) -> Void in
+            if let textFieled = (alert.textFields?.first)! as? UITextField {
+                self.getWeatherFor(textFieled.text!)
+            }
+        }
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        alert.addTextFieldWithConfigurationHandler { (textFiled) -> Void in
+            textFiled.placeholder = "Enter city name"
+        }
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    func getWeatherFor(city : String) {
+       print(city)
+    }
 }
 
